7d706c69377b2ae0d2a84fb57e827b7f620d91df
Kleber Silva <kb-max@hotmail.com>
2018-08-13 20:25:23 -0300

    Created delete method and exception handled

--- app/Services/PacoteService.php
+++ app/Services/PacoteService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use App\Repositories\PacoteRepositoryInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class PacoteService
 {
@@ -16,27 +17,61 @@ class PacoteService
 
     public function buscarTodosPacotes()
     {
-        $pacotes = $this->pacoteRepository->buscarTodosPacotes();
-
-        if (count($pacotes) > 0) {
-            return $pacotes;
-        } else {
-            return array();
+        try {
+            $pacotes = $this->pacoteRepository->buscarTodosPacotes();
+    
+            if (count($pacotes) > 0) {
+                return response()->json($pacotes, Response::HTTP_OK);
+            } else {
+                return response()->json([], Response::HTTP_OK);
+            }
+        } catch(QueryException $e){
+            return response()->json(['erro'=> 'Erro de conexão com o banco de dados.'], Reponse::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
     public function buscarPacote($id)
     {
-        return $this->pacoteRepository->buscarPacote($id);
+        try {
+            return $this->pacoteRepository->buscarPacote($id);
+
+            if (count($pacote) > 0){
+                return response()->json($pacote, Response::HTTP_OK);
+            } else {
+                return response()->json(null, Response::HTTP_NOT_FOUND);
+            }
+        } catch(QueryException $e){
+            return response()->json(['erro'=> 'Erro de conexão com o banco de dados.']);
+        }
     }
 
     public function criarPacote(Request $request)
     {
-        return $this->pacoteRepository->criarPacote($request);
+        try {
+            $pacote = $this->pacoteRepository->criarPacote($request);
+            return response()->json($pacote, Response::HTTP_CREATED);
+        }catch(QueryException $e) {
+            return response()->json(['erro'=> 'Erro de conexão com o banco de dados.']);
+        }
     }
 
     public function editarPacote(int $id, Request $request)
     {
-        return $this->pacoteRepository->editarPacote($id, $request);
+        try {
+            $pacote =  $this->pacoteRepository->editarPacote($id, $request);
+            return response()->json($pacote, Reponse::HTTP_OK);
+        } catch(QueryException $e){
+            return response()->json(['erro'=> 'Erro de conexão com o banco de dados.']);
+        }
+    }
+
+    public function excluirPacote($id)
+    {
+        try {
+            $pacote = $this->pacoteRepository->excluirPacote($id);
+            return response()->json(null, Reponse::HTTP_OK);
+        }catch(QueryException $e){
+            return response()->json(['erro'=> 'Erro de conexãp com o banco de dados.']);
+        }
     }
 }
